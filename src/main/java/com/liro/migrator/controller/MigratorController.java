6fc96d8908ae5da1c979e80574e19f800c74f065
franbusleiman <franbusleiman@gmail.com>
2024-09-03 22:49:36 -0300

    c

--- src/main/java/com/liro/migrator/controller/MigratorController.java
+++ src/main/java/com/liro/migrator/controller/MigratorController.java
@@ -4,10 +4,7 @@ import com.liro.migrator.service.ClientsMigrator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,8 +20,8 @@ public class MigratorController {
         this.clientsMigrator = clientsMigrator;
     }
 
-    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Void> createRecord(MultipartFile file,
+    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE}, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> createRecord(@RequestPart(required = true) MultipartFile file,
                                              @RequestParam("vetUserId") Long vetUserId) throws IOException {
         clientsMigrator.migrate(vetUserId, file);
 

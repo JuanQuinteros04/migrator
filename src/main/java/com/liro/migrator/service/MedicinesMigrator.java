4a02d2a14d59f08ef61cd44c8d3dc772e539f4cb
franbusleiman <franbusleiman@gmail.com>
2024-09-25 10:14:30 -0300

    c

--- src/main/java/com/liro/migrator/service/MedicinesMigrator.java
+++ src/main/java/com/liro/migrator/service/MedicinesMigrator.java
@@ -4,13 +4,18 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.liro.migrator.dtos.MedicineDTO;
+import com.liro.migrator.dtos.enums.AnimalType;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class MedicinesMigrator {
@@ -26,8 +31,38 @@ public class MedicinesMigrator {
 
     public void migrateMedicines(MultipartFile file) throws IOException {
 
-        List<MedicineDTO> medicineDTOs = readCsvFile(file, MedicineDTO.class);
 
-        medicineDTOs.forEach(System.out::println);
+        Reader reader = new InputStreamReader(file.getInputStream());
+
+        CsvToBean<MedicineDTO> csvToBean = new CsvToBean<MedicineDTO>();
+
+        Map<String, String> columnMapping = new HashMap<String, String>();
+        columnMapping.put("Nombre Comercial", "commercialName");
+        columnMapping.put("Nombre científico", "formalName");
+        columnMapping.put("Uso", "onlyVetUse");
+        columnMapping.put("Tipo de venta", "needPrescription");
+        columnMapping.put("Conservación", "conservation");
+        columnMapping.put("Contenido", "content");
+        columnMapping.put("Unidad Dosis", "dosesUnity");
+        columnMapping.put("Marca", "brandName");
+        columnMapping.put("Uso en", "animalType");
+        columnMapping.put("Tipo", "medicineTypeId");
+        columnMapping.put("SubTipo", "medicineGroups");
+        columnMapping.put("Presentación", "presentationName");
+
+
+
+
+        HeaderColumnNameTranslateMappingStrategy<MedicineDTO> strategy = new HeaderColumnNameTranslateMappingStrategy<MedicineDTO>();
+        strategy.setType(MedicineDTO.class);
+        strategy.setColumnMapping(columnMapping);
+
+        List<MedicineDTO> list = null;
+        CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
+        csvToBean.setCsvReader(csvReader);
+        csvToBean.setMappingStrategy(strategy);
+        list = csvToBean.parse();
+
+        list.forEach(System.out::println);
     }
 }

c488753089dd7c22acaba9ad1bdd7d293cee9e91
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-10-16 23:15:09 -0300

    Creating ApplicationsMigrator

--- src/main/java/com/liro/migrator/service/ApplicationsMigrator.java
+++ src/main/java/com/liro/migrator/service/ApplicationsMigrator.java
@@ -1,23 +1,37 @@
 package com.liro.migrator.service;
 
+import com.liro.migrator.config.FeignMedicineClient;
+import com.liro.migrator.dtos.AnimalMigrationResponse;
 import com.liro.migrator.dtos.ApplicationRecordDTO;
 import com.liro.migrator.dtos.MedicineDTO;
+import com.opencsv.CSVParser;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Service
 public class ApplicationsMigrator {
 
-    public void migrateApplications(MultipartFile file) throws IOException, CsvException {
-        Map<String, Integer> fields = new HashMap<>();
+    @Autowired
+    FeignMedicineClient feignMedicineClient;
+    private final DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+
+    public void migrateApplications(MultipartFile file, Long vetUserId, List<AnimalMigrationResponse> animalMigrationResponses) throws IOException, CsvException {
+
         List<ApplicationRecordDTO> applicationRecordDTOS = new ArrayList<>();
 
+        Map<String, Integer> fields = new HashMap<>();
+
+
+        // Leer el archivo CSV (vacunas)
         try (CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream()))) {
 
 
@@ -27,31 +41,79 @@ public class ApplicationsMigrator {
 
             for (int i = 0; i < firstLine.length; i++) {
                 fields.put(firstLine[i], i);
+
+                while (iterator.hasNext()) {
+                    String[] row = iterator.next();
+
+                    String codigoPaciente = row[fields.get("Codigopaci")];
+
+
+                    Optional<AnimalMigrationResponse> animal = animalMigrationResponses.stream()
+                            .filter(animalMigrationResponse -> animalMigrationResponse.getVetterCode().equals(vetUserId + "-" + codigoPaciente))
+                            .findFirst();
+
+
+                    ApplicationRecordDTO applicationDTO = ApplicationRecordDTO.builder()
+                            .animalId(row[fields.get(animal)])// De donde sale? se supone que ya esta en la DB
+                            .medicineId(medicineIdConverter(row[fields.get("vacuna")]))
+                            .applicationDate(dateConverter(row[fields.get("fecha_apli")]))
+                            .endDate(dateConverter(row[fields.get("fecha_reva")]))
+                            .build();
+
+                    applicationRecordDTOS.add(applicationDTO);
+                }
+
+                feignMedicineClient.createApplications(applicationRecordDTOS);
             }
-
-//            while (iterator.hasNext()) {
-//                String[] row = iterator.next();
-//
-//                applicationRecordDTOS.add(MedicineDTO.builder()
-//                        .brandName(row[fields.get("Marca")])
-//                        .medicineTypeId(medicineTypeConverter(row[fields.get("Tipo")]))
-//                        .presentationName(row[fields.get("Presentación")])
-//                        .animalType(animalTypeConverter(row[fields.get("Uso en")]))
-//                        .dosesUnity(row[fields.get("Unidad Dosis")])
-//                        .commercialName(row[fields.get("Nombre comercial")])
-//                        .formalName(row[fields.get("Nombre científico")])
-//                        .onlyVetUse(onlyVetUseConverter(row[fields.get("Uso")]))
-//                        .medicineGroups(medicinesGroupsConverter(row[fields.get("SubTipo")]))
-//                        .needPrescription(needPrescriptionConverter(row[fields.get("Tipo de venta")]))
-//                        .build());
-//
-//            }
-
-//            feignMedicineClient.createMedicines(medicineDTOS);
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
         }
     }
 
+
+
+    private Long medicineIdConverter(String name){
+        switch (name){
+            case "CUADRUPLE":
+                return 960L;
+
+            case "DHPPI":
+                return 123L;
+
+            case "LEPTOSPIRA":
+                return 959L;
+
+            case "MOQUILLO":
+                return 958L;
+
+            case "OCTUVALENTE":
+                return 957L;
+
+            case "PARVOVIRUS":
+                return 956L;
+
+            case "PUPPY":
+                return 955L;
+
+            case "ATP EXTERNO":
+                return 961L;
+
+            case "ATP INYECTABLE":
+                return 962L;
+
+            case "ATP COMPRIMIDOS":
+                return 963L;
+
+            default:
+                return null;
+        }
+    }
+
+    private String dateConverter(String date){
+
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy"); // Formato de entrada
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // Formato de salida
+
+        LocalDate applicationDate = LocalDate.parse(date, inputFormatter);
+
+        return applicationDate.format(outputFormatter);
+    }
 }
